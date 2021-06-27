4a2b6e132fa5940ae8a7b0eb1eb578783e812acd
edearth <nepormoseno1992@gmail.com>
2021-06-27 19:50:10 +0200

    First app draft
    It includes QR code and UUID generation. It also shows an initial
    design, but there's a bunch of stuff that needs to be added/tweaked
    (the list elements look bad, styles are applied to elements and not
    classes, the whole thing is white with black text, etc.)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import dragArrow from './dragArrow.svg';
 import './App.css';
+import QRCode from "react-qr-code";
+import uuid from "react-uuid";
+
+var user = {
+  id : uuid(),
+  name : "Tester Test"
+}
+
+function LoyaltyCardItem(props) {
+  return (
+    <div style={{height:"80px", borderStyle:"1px solid black"}}>
+      <img height="80px" src={props.store.picture} style= {{float:"left"}} alt={props.store.name + "'s picture"} />
+      <div>
+        <h2>{props.store.name}</h2>
+        <p>{props.store.stampsCollected} / {props.store.stampsRequired}</p>
+      </div>
+    </div>
+  )
+}
+
+function LoyaltyCardList(props) {
+  let response = mockedListResponse();
+  return (
+    <div>
+      { response.stores.map ( store =>
+          <LoyaltyCardItem store={store} />
+        )
+      }
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{height:"100vh"}}>
+        <div style={{display:"flex", height:"80%", justifyContent:"center", alignItems:"center"}}>
+          <QRCode value={user.id} size={window.screen.width - 20}/>
+        </div>
+        <div style={{position:"absolute", bottom:"5%", width:"100%"}}>
+          <p>Welcome back, {user.name}!</p>
+          <img height="30px" src={dragArrow} />
+        </div>
+      </div>
+      <LoyaltyCardList />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+function mockedListResponse() {
+  return {
+    stores : [
+      {
+        name: "Store 1",
+        picture: "https://cdn2.thecatapi.com/images/twnYLeQRZ.png",
+        stampsCollected: 3,
+        stampsRequired: 10
+      },
+      {
+        name: "Store 2",
+        picture: "https://cdn2.thecatapi.com/images/twnYLeQRZ.png",
+        stampsCollected: 15,
+        stampsRequired: 20
+      },
+      {
+        name: "Store 3",
+        picture: "https://cdn2.thecatapi.com/images/twnYLeQRZ.png",
+        stampsCollected: 8,
+        stampsRequired: 10
+      }
+    ]
+  };
+}
